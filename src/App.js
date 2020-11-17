22bbecfbd250b090b85d9a90f4d2d1d424d2a94f
kimchanmin <rlacksals96@gmail.com>
2020-11-18 00:08:40 +0900

    add:route to bulletin board

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ class App extends Component {
             <div>
                 <Switch>
                     <Route path="/" exact={true} component={Main} />
+
                     <Route path="/auth/login" exact={true} component={Login} />
                     <Route path="/auth/signup" exact={true} component={SignUp} />
                     <Route path="/rooms" exact={true} component={Rooms}/>
                     <Route path="/rooms/:roomId" exact={true} component={Room}/>
-                    <Route path="/rooms/:roomId/:option" exact={true} component={BulletinBoard}/>
+                    <Route path="/rooms/:roomId/board" exact={true} component={BulletinBoard}/>
                     <Route component={NotFound} />
                 </Switch>
 
