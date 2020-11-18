f2bc07569828a01facdcb66da25811988c193ae0
kimchanmin <rlacksals96@gmail.com>
2020-11-18 22:44:46 +0900

    add:add axios in login component
    현재 추가중이긴 하나, 아직 로직이 정확히 파악이 안됨..
    -> array 추가/삭제/수정
    -> history
    -> axios 사용법에 대해 다시 공부하고 로직 추가해야함.
    
    완성 불가하다고 판단될시 원상복구 하고 싶으면 develop branch로 복구할 것!

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,8 @@ import React,{useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import Header from "../components/layout/Header";
 import LoginForm from "../components/login/LoginForm";
-function Login(){
+import axios from 'axios';
+function Login(props){
     const [inputs,setInputs]=useState({
         userId:'',
         userPw:''
@@ -21,24 +22,41 @@ function Login(){
         console.log(userId,userPw);
 
     }
+    const handleSubmit=(e)=>{
+
+      axios
+        .post(
+          "http://localhost:3000/login",
+          {
+            user:{
+              userId,
+              userPw
+            }
+          },
+          {withCredentials:true}
+        )
+        .then(response=>{
+          if(response.data.status==="created"){
+            props.handleSuccessfulAuth(response.data);
+          }
+        })
+        .catch(error=>{
+          console.log("registration error",error);
+        });
+
+
+    }
     return (
         <div>
+          <form onSubmit={handleSubmit}>
             <Header/>
             <h3 id="title">Login</h3>
             <LoginForm userId={userId} userPw={userPw} onChange={onChange}/>
-            <button onClick={onCreate}>Login</button>
+            <button type="submit" onClick={onCreate}>Login</button>
+            {/*onClick과 관련 함수는 사라져야할 것임..*/}
 
-            <p>지워져야 할 것이나 현재는 확인 수단이 없어서 밑에꺼 누르면 바로 방으로 이동</p>
             <Link to="/rooms">로그인</Link>
-
-
-
-
-
-
-
-
-
+          </form>
         </div>
 
 
