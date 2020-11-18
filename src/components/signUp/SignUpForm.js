965b59043104486169c07b9133c0c0cd2d73fbb4
김찬민 <rlacksals96@gmail.com>
2020-11-18 16:29:26 +0900

    add:login,pw form

---
+++ src/components/signUp/SignUpForm.js
@@ -0,0 +1,62 @@
+import React,{useState,useEffect} from 'react';
+import '../../styles/SignUp.css'
+const SignUpForm=({userId,userPw,userPwRe,email,onChange})=>{
+
+    const checkId=()=>{
+
+    }
+
+    const renderPwDuplicateMsg=()=>{
+        if(userPw==='')
+            return '';
+        else{
+            return userPw===userPwRe? <span>일치합니다</span>:<span>일치하지 않습니다</span>
+        }
+    }
+    return(
+        <div>
+            <div>
+                <p>사용할 아이디를 입력하세요</p>
+                <input type="text"
+                       name="userId"
+                       value={userId}
+                       onChange={onChange}
+                       placeholder="user id"
+                />
+                <span>&nbsp;</span>
+                <button onClick={checkId}>중복확인</button>
+            </div>
+            <div>
+                <p>사용할 비밀번호를 입력하세요</p>
+                <input type="password"
+                       name="userPw"
+                       value={userPw}
+                       onChange={onChange}
+                       placeholder="password"
+                />
+            </div>
+            <div>
+                <p>비밀번호를 다시 입력해주세요</p>
+                <input type="password"
+                       name="userPwRe"
+                       value={userPwRe}
+                        onChange={onChange}
+                       placeholder="password again..."
+                />
+                <span className="pwCheck">&nbsp;{renderPwDuplicateMsg()}</span>
+            </div>
+            <div>
+                <p>이메일을 입력해주세요</p>
+                <input type="email"
+                       name="email"
+                       value={email}
+                       onChange={onChange}
+                       placeholder="email"
+                />
+            </div>
+            <br/>
+        </div>
+    )
+}
+
+export default SignUpForm;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from '../components/layout/Header';
 import {Link} from 'react-router-dom';
-
+import '../styles/Main.css'
 
 const Main = () => {
     return (
         <div>
             <Header/>
             <h1>welcome to study with me</h1>
-            <Link to="./auth/login"><button>login page</button></Link>
-            <Link to="./auth/signup"><button>sign up page</button></Link>
+            <div className="button-container">
+                <Link to="./auth/login"><button>login page</button></Link>
+                <Link to="./auth/signup"><button>sign up page</button></Link>
+
+            </div>
 
 
 
