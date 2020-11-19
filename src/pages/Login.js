88d38527d6f1fa1744419765f40252e181549621
kimchanmin <rlacksals96@gmail.com>
2020-11-19 15:30:50 +0900

    add:login,signUp..send info to url

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,56 @@
-import React,{useState,useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/layout/Header";
 import LoginForm from "../components/login/LoginForm";
-import axios from 'axios';
-function Login(props){
-    const [inputs,setInputs]=useState({
-        userId:'',
-        userPw:''
-    });
-    const {userId,userPw}=inputs;
-    const onChange=(e)=>{
-        const {name,value}=e.target;
-        setInputs({...inputs,[name]:value});
-    }
-    const onCreate=()=>{
-        const user={
-            userId,
-            userPw
-        };
-        //this is for check
-        console.log(userId,userPw);
+import axios from "axios";
+function Login(props) {
+	const [inputs, setInputs] = useState({
+		userId: "",
+		userPw: "",
+	});
 
-    }
-    const handleSubmit=(e)=>{
+	const { userId, userPw } = inputs;
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({ ...inputs, [name]: value });
+	};
 
-      axios
-        .post(
-          "http://localhost:3000/login",
-          {
-            user:{
-              userId,
-              userPw
-            }
-          },
-          {withCredentials:true}
-        )
-        .then(response=>{
-          if(response.data.status==="created"){
-            props.handleSuccessfulAuth(response.data);
-          }
-        })
-        .catch(error=>{
-          console.log("registration error",error);
-        });
+	const handleSubmit = (e) => {
+		const test = {
+			userId: userId,
+			title: userPw,
+		};
+		const user = {
+			userId: userId,
+			userPw: userPw,
+		};
+		const url = "https://jsonplaceholder.typicode.com/posts";
 
+		// 1. url 지정
+		// 2. user로 교체하면 됨.
 
-    }
-    return (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <Header/>
-            <h3 id="title">Login</h3>
-            <LoginForm userId={userId} userPw={userPw} onChange={onChange}/>
-            <button type="submit" onClick={onCreate}>Login</button>
-            {/*onClick과 관련 함수는 사라져야할 것임..*/}
-
-            <Link to="/rooms">로그인</Link>
-          </form>
-        </div>
-
-
-    )
+		e.preventDefault();
+		console.log(test); //넘어가는값 확인용. 추후 삭제
+		axios
+			.post(url, test)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log("registration error", error);
+			});
+	};
+	return (
+		<div>
+			<Header />
+			<h3 id="title">Login</h3>
+			<form onSubmit={handleSubmit}>
+				<LoginForm userId={userId} userPw={userPw} onChange={onChange} />
+				<button type="submit">Login</button>
+			</form>
+			<br />
+			<Link to="/rooms">link to room</Link>
+		</div>
+	);
 }
 export default Login;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,40 +1,52 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import Header from "../components/layout/Header";
 import SignUpForm from "../components/signUp/SignUpForm";
-function SignUp(){
-    const [inputs,setInputs]=useState({
-        userId:'',
-        userPw:'',
-        userPwRe:'',
-        email:''
-    });
-    const {userId,userPw,userPwRe,email}=inputs;
+import axios from "axios";
+function SignUp() {
+	const [inputs, setInputs] = useState({
+		userId: "",
+		userPw: "",
+		userPwRe: "",
+		email: "",
+	});
+	const { userId, userPw, userPwRe, email } = inputs;
 
-    const onChange=(e)=>{
-        const {name,value}=e.target;
-        setInputs({...inputs,[name]:value});
-    }
-    const onCreate=()=>{
-        const user={
-            userId,
-            userPw,
-            userPwRe,
-            email
-        }
-        //this is for check
-        console.log(userId,userPw,userPwRe,email)
-    }
-    return(
-        <div>
-            <Header/>
-            <SignUpForm userId={userId}
-                        userPw={userPw}
-                        userPwRe={userPwRe}
-                        email={email}
-                        onChange={onChange}
-            />
-            <button onClick={onCreate}>Sign Up</button>
-        </div>
-    )
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({ ...inputs, [name]: value });
+	};
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const test = {
+			userId: { userId },
+			title: { userPw },
+		};
+		const user = {
+			userId: { userId },
+			userPw: { userPw },
+			email: { email },
+		};
+		const url = "https://jsonplaceholder.typicode.com/posts";
+		console.log(test); //넘어가는 값 확인용. 추후 삭제
+		axios
+			.post(url, test)
+			.then((response) => console.log(response))
+			.catch((error) => console.lor("error: ", error));
+	};
+	return (
+		<div>
+			<Header />
+			<form onSubmit={submitHandler}>
+				<SignUpForm
+					userId={userId}
+					userPw={userPw}
+					userPwRe={userPwRe}
+					email={email}
+					onChange={onChange}
+				/>
+				<button type="submit">Sign Up</button>
+			</form>
+		</div>
+	);
 }
 export default SignUp;
