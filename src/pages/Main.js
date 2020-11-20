6c8ec1c647e3fbb76278c5405b6929b7c3f672dc
yusu <usujang@naver.com>
2020-11-20 13:30:11 +0900

    메인화면 이미지 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from '../components/layout/Header';
 import {Link} from 'react-router-dom';
+import imgA from '../images/study_with_me.jpg';
 
 import "../styles/Main.css";
 
@@ -10,7 +11,8 @@ const Main = () => {
     return (
         <div>
             <Header/>
-            <h1>welcome to study with me</h1>
+            <img src={imgA} />  
+            <h1 className="desc">welcome to study with me</h1>
             <div className="button-container">
                 <Link to="./auth/login"><button className="loginButton">login page</button></Link>
                 <Link to="./auth/signup"><button className="singUpButton">sign up page</button></Link>
