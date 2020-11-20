6cdcf67752522340dcfb595abb994b793abc6d7b
kimchanmin <rlacksals96@gmail.com>
2020-11-20 15:46:30 +0900

    add:logout btn..clear localStorage and move to home

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import '../../styles/Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Header.css";
 
 const Header = ({ onLogout }) => (
-    <div className="header">
-        <Link to={"/"}>
-            <div className="logo">LOGO</div>
-        </Link>
-        {/*<div className="logout">*/}
-        {/*    logout..?*/}
-        {/*</div>*/}
-    </div>
+	<div className="header">
+		<Link to={"/"}>
+			<div className="logo">LOGO</div>
+		</Link>
+		<Link to={"/"}>
+			<button className="logoutBtn" onClick={handleLogout}>
+				logout
+			</button>
+		</Link>
+	</div>
 );
+
+const handleLogout = () => {
+	localStorage.clear();
+	alert("Logout 되었습니다");
+};
 
 export default Header;
